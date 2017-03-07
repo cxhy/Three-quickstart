464393df1edaf9f9d8de46e685e158ecdeeeb23a
cxhy <979009228@qq.com>
2017-03-08 07:58:17 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
 function init(){
+  var renderer = new THREE.WebGLRenderer({
+      canvas: document.getElementById('mainCanvas')
+  });
+  
+  renderer.setClearColor(0x000000);
+  
+  var scene = new THREE.Scene();
+  
+  var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 1000);
+  camera.position.set(0, 0, 5);
+  scene.add(camera);
+  
+  var xx = new THREE.Mesh(new THREE.CubeGeometry(1, 2, 3),
+          new THREE.MeshBasicMaterial({
+              color: 0xffe000
+          })
+  );
+  scene.add(xx);
+  
+  renderer.render(scene, camera);
 
 }
-
-var renderer = new THREE.WebGLRenderer({
-    canvas: document.getElementById('mainCanvas')
-});
-
-renderer.setClearColor(0x000000);
-
-var scene = new THREE.Scene();
-
-var camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 1000);
-camera.position.set(0, 0, 5);
-scene.add(camera);
-
-var xx = new THREE.Mesh(new THREE.CubeGeometry(1, 2, 3),
-        new THREE.MeshBasicMaterial({
-            color: 0xffe000
-        })
-);
-scene.add(xx);
-
-renderer.render(scene, camera);
