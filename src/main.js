10adc2955b8b51da7a61747f30a2630114730f15
cxhy <979009228@qq.com>
2017-03-14 23:02:34 +0800

    daily update

--- src/main.js
+++ src/main.js
@@ -8,14 +8,14 @@ function init(){
   var scene = new THREE.Scene();
   
     //var camera = new THREE.OrthographicCamera(-2, 2, 1.5, -1.5, 1, 10);
-    var camera = new THREE.PerspectiveCamera(60, 400/300, 1, 10);      
+    var camera = new THREE.PerspectiveCamera(45, 400/300, 1, 10);      
     camera.position.set(0, 0, 5);
     //camera.lookAt(new THREE.Vector3(0,0,0))
   scene.add(camera);
   
   var xx = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1),
           new THREE.MeshBasicMaterial({
-              color: 0xffaa00,
+              color: 0xff0000,
               wireframe: true
           })
   );
